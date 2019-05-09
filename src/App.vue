50784e580b1e11a562787eea719fcb90f3fd549a
Tom Needham <tom@owncloud.com>
2019-05-09 19:12:05 +0100

    Add vue app with leaflet and hotline

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div style="height: 1000px; width: 100%">
+      <l-map
+      style="height: 80%; width: 100%"
+      :zoom="zoom"
+      :center="center"
+      @update:zoom="zoomUpdated"
+      @update:center="centerUpdated"
+      @update:bounds="boundsUpdated"
+      >
+        <l-tile-layer :url="url"></l-tile-layer>
+        <v-hotline :latlngs="latLngArray" :min="minValue" :max="maxValue"></v-hotline>
+      </l-map>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { LMap, LTileLayer } from 'vue2-leaflet'
+import Vue2LeafletHotline from 'vue2-leaflet-hotline'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    'l-map': LMap,
+    'l-tile-layer': LTileLayer,
+    'v-hotline': Vue2LeafletHotline
+  },
+  data () {
+    return {
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      zoom: 3,
+      center: [47.413220, -1.219482],
+      bounds: null,
+      latLngArray: [[47.413220, -1.219482, 2], [47.413230, -1.219492, 2]]
+    };
+  },
+  methods: {
+    zoomUpdated (zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated (center) {
+      this.center = center;
+    },
+    boundsUpdated (bounds) {
+      this.bounds = bounds;
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import 'leaflet/dist/leaflet.css'
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
